86864738455de8b434e53b4d6a59583d5785ffbe
Kai <kaixun@baojiayin.com>
2016-07-22 11:59:42 +0800

    add style, readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# react-demo-douban-book-search
+# About
+
 A book search app using douban api that demonstrate the basics of React
+
+# Run
+
+```sh
+npm install
+npm run build
+python -m SimpleHTTPServer
+```
+Available at http://127.0.0.1:8000
--- main.js
+++ main.js
@@ -4,7 +4,9 @@ let ReactDom = require('react-dom');
 class Logo extends React.Component{
   render(){
     return (
-      <img src="https://img3.doubanio.com/f/shire/8308f83ca66946299fc80efb1f10ea21f99ec2a5/pics/nav/lg_main_a11_1.png"/>
+      <div className="logo">
+        <img src="https://img3.doubanio.com/f/shire/8308f83ca66946299fc80efb1f10ea21f99ec2a5/pics/nav/lg_main_a11_1.png"/>
+      </div>
     );
   }
 }
@@ -18,12 +20,14 @@ class SearchBox extends React.Component{
     return (
       <div>
         <input
+          className="search-box"
           type="text"
           placeholder="请输入搜索关键词"
           value={this.props.keyword}
           ref="KeywordTextInput"
           onChange={this.handleChange}/>
         <input
+          className="filter"
           type="checkbox"
           checked={this.props.highRatingOnly}
           ref="highRatingOnlyInput"
@@ -50,7 +54,7 @@ class BookList extends React.Component{
     }).map((book) => {
       return (
         <li key={book.id}>
-          <img src={book.image}/>
+          <a href={book.alt} target="_blank"><img src={book.image}/></a>
           <p>{book.title}</p>
           <p>{book.author}</p>
           <p>{book.rating.average}</p>
@@ -58,7 +62,7 @@ class BookList extends React.Component{
       );
     });
     return (
-      <ul>
+      <ul className="book-list">
         {books}
       </ul>
     );
@@ -100,7 +104,7 @@ class DoubanBookSearch extends React.Component{
   }
   render(){
     return (
-      <div>
+      <div id="douban-search-app">
         <Logo/>
         <SearchBox keyword={this.state.keyword} highRatingOnly={this.state.highRatingOnly} onUserInput={this.handleUserInput} />
         <BookList books={this.state.books} highRatingOnly={this.state.highRatingOnly}/>
